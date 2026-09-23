db175023c4a10461e9deab5a4f8460f5fcfdb78b
JanFaltynekVUT <qmfaltynek@vutbr.cz>
2026-09-23 14:54:02 +0200

    Delete Clean Code directory

--- Clean Code/CleanCode/CASE1-NAMING/1_NoContextName.cs
+++
@@ -1,9 +0,0 @@
-// Clean code case: use names that communicate intent instead of single letters.
-// A tiny `d` makes readers reverse-engineer meaning, while a descriptive name tells the story upfront.
-
-// Bad
-int d;
-        
-// Better
-int daySinceLastOrder;
-    
